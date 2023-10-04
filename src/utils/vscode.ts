69a80b669d1f7aa3a312b61b97ab23ebfe7529c4
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-10-04 10:07:03 +0530

    add toast to browser

--- src/utils/vscode.ts
+++ src/utils/vscode.ts
@@ -1,6 +1,7 @@
 import type { WebviewApi } from "vscode-webview";
 import type { BoardType, MessageType } from "@/interface";
 import { defaultBoard } from "./data";
+import { toast as _toast } from "react-toastify";
 
 class VSCodeAPIWrapper {
   private readonly vsCodeApi: WebviewApi<unknown> | undefined;
@@ -11,11 +12,19 @@ class VSCodeAPIWrapper {
     }
   }
 
-  public postMessage(message: MessageType) {
+  private _postMessage(message: MessageType) {
     if (this.vsCodeApi) {
       this.vsCodeApi.postMessage(message);
     } else {
       console.log(message);
+    }
+  }
+
+  public toast(text: string, type: "info" | "warn" | "error" | "success" = "info") {
+    if (this.vsCodeApi) {
+      this._postMessage({ action: type, text });
+    } else {
+      _toast[type](text);
     }
   }
 
@@ -29,7 +38,7 @@ class VSCodeAPIWrapper {
    */
   public async getState(): Promise<BoardType> {
     if (this.vsCodeApi) {
-      this.postMessage({ action: "load" });
+      this._postMessage({ action: "load" });
       return new Promise((res) => {
         const onMessageReceive = ({ data: message }: MessageEvent<MessageType>) => {
           window.removeEventListener("message", onMessageReceive);
@@ -58,7 +67,7 @@ class VSCodeAPIWrapper {
    */
   public setState(newState: BoardType) {
     if (this.vsCodeApi) {
-      this.postMessage({ action: "save", data: newState });
+      this._postMessage({ action: "save", data: newState });
     } else {
       localStorage.setItem("vscodeState", JSON.stringify(newState));
     }
