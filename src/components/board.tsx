8d0649f27ecf7762b00fc85b17c70cdf58ebff7a
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-12-13 06:48:17 +0530

    clean up

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -4,39 +4,12 @@ import ColumnList from "./column-list";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { ForkMe } from "@mayank1513/fork-me/server";
 import "@mayank1513/fork-me/server/index.css";
+import { BoardType } from "@/interface";
 
 export default function Board() {
-  const { setState, state } = useGlobalState();
+  const { state, setState } = useGlobalState();
 
-  const onDragEnd = (result: DropResult) => {
-    const { source, destination, draggableId } = result;
-    if (!destination) return;
-
-    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
-
-    /** column id always starts with "column-" */
-    const columns = [...state.columns];
-    if (draggableId.startsWith("column-")) {
-      const column = columns.splice(source.index, 1)[0];
-      columns.splice(destination.index, 0, column);
-      setState({ ...state, columns: columns });
-    } else {
-      const sourceCol = columns.find((c) => c.id === source.droppableId);
-      let destinationCol;
-      if (destination.droppableId === "columns") {
-        destinationCol = columns[Math.min(destination.index, columns.length - 1)];
-      } else {
-        destinationCol = columns.find((c) => c.id === destination.droppableId);
-      }
-      const taskId = sourceCol?.tasksIds.splice(source.index, 1)[0];
-      const tasks = { ...state.tasks };
-      if (taskId) {
-        tasks[taskId].columnId = destination.droppableId;
-        destinationCol?.tasksIds.splice(destination.index, 0, taskId);
-        setState({ ...state, columns });
-      }
-    }
-  };
+  const onDragEnd = (result: DropResult) => handleDragEnd(result, state, setState);
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className={[styles.board, styles.dark].join(" ")}>
@@ -45,20 +18,54 @@ export default function Board() {
           <hr />
         </header>
         <ColumnList columns={state.columns} />
-        {state.scope === "Browser" && (
-          <>
-            <ForkMe
-              gitHubUrl="https://github.com/mayank1513/vscode-extension-trello-kanban-board"
-              noAutoFork
-              textColor="lightgreen"
-              bgColor="orangered"
-            />
-            <a href="vscode:extension/mayank1513.trello-kanban-task-board" className={styles.install}>
-              Install VSCode Extension
-            </a>
-          </>
-        )}
+        {state.scope === "Browser" && <BrowserOnlyUI />}
       </div>
     </DragDropContext>
+  );
+}
+
+function handleDragEnd(result: DropResult, state: BoardType, setState: (state: BoardType) => void) {
+  const { source, destination, draggableId } = result;
+  if (!destination) return;
+
+  if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+  /** column id always starts with "column-" */
+  const columns = [...state.columns];
+  if (draggableId.startsWith("column-")) {
+    const column = columns.splice(source.index, 1)[0];
+    columns.splice(destination.index, 0, column);
+    setState({ ...state, columns: columns });
+  } else {
+    const sourceCol = columns.find((c) => c.id === source.droppableId);
+    let destinationCol;
+    if (destination.droppableId === "columns") {
+      destinationCol = columns[Math.min(destination.index, columns.length - 1)];
+    } else {
+      destinationCol = columns.find((c) => c.id === destination.droppableId);
+    }
+    const taskId = sourceCol?.tasksIds.splice(source.index, 1)[0];
+    const tasks = { ...state.tasks };
+    if (taskId) {
+      tasks[taskId].columnId = destination.droppableId;
+      destinationCol?.tasksIds.splice(destination.index, 0, taskId);
+      setState({ ...state, columns });
+    }
+  }
+}
+
+function BrowserOnlyUI() {
+  return (
+    <>
+      <ForkMe
+        gitHubUrl="https://github.com/mayank1513/vscode-extension-trello-kanban-board"
+        noAutoFork
+        textColor="lightgreen"
+        bgColor="orangered"
+      />
+      <a href="vscode:extension/mayank1513.trello-kanban-task-board" className={styles.install}>
+        Install VSCode Extension
+      </a>
+    </>
   );
 }
