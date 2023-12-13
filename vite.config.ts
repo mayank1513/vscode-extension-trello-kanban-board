86943cf43d67ab4e7cdce1b2f6ff615e8625a301
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-12-13 10:55:12 +0530

    Update test config

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,9 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html", "clover"],
+    },
   },
 });
