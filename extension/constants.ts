e9423921c70a52046a4db67f7111eafa14044116
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-01-08 13:30:35 +0530

    save to and load from File

--- extension/constants.ts
+++ extension/constants.ts
@@ -1,3 +1,4 @@
 export const scopes = ["Workspace", "Global"] as const;
 export type ScopeType = (typeof scopes)[number];
 export const prefix = "mayank1513.trello-kanban.";
+export const IGNORED = "--ignored--";
